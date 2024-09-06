07ce7002fb67602009487fef143e73fcdc94fd29
saikiran123in@gmail.com <saikiran123in@gmail.com>
2024-09-06 21:56:12 +0530

    chore: removed logo for csanvas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import RectangleBox from './Shapes';
+import RectangleBox from "./Shapes";
 
 function App() {
   return (
     <div className="App ">
-     {/* <Canvas/> */}
-        <RectangleBox/>
+      {/* <Canvas/> */}
+      <RectangleBox />
     </div>
   );
 }
